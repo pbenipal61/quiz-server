af44a00ae1a7667e8b385ed36d8c5ef0bba1a460
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-03-18 00:04:03 +0200

    Added HTTP support with HTTPS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const https = require("https");
 const fs = require("fs");
 const app = require('./app');
 const port = process.env.PORT || 80;
-
+const https_port = 443;
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/postmaninteractive.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/postmaninteractive.com/cert.pem', 'utf8');
@@ -15,8 +15,13 @@ const credentials = {
     ca: ca
 };
 
+const http_server = http.createServer(app);
+http_server.listen(port, () => {
+    console.log("HTTP server up");
+});
 
 const server = https.createServer(credentials, app);
-server.listen(port, () => {
-    console.log(`App running on port ${port}`);
+server.listen(https_port, () => {
+    console.log(`HTTPS server up`);
 });
+
